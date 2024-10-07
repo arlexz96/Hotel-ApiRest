3eaff8f4dd48258be604f0e0dae7c5d07ee4dbc8
arlexz96 <arlex.z96@gmail.com>
2024-10-07 00:56:39 -0500

    first commit

---
+++ Controllers/v1/cualquier/Cualquier.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaDesempenoApi.Controllers.v1.cualquier
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class Cualquier : ControllerBase
+    {
+        
+    }
+}
